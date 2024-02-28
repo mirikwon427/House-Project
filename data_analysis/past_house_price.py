7246da146b85dd5a18d3931e34a127e5e4b8e4d1
soyeun-data <thduswkd96@gmail.com>
2024-02-28 12:33:30 +0900

    past house price & API & predict house price codes

---
+++ data_analysis/past_house_price.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def past_price(data):
+    from datetime import date
+
+    today = date.today()
+    today_formatted = today.strftime("%Y%m%d")
+    df = pd.read_csv('./data/API_data_{}.csv'.format(today_formatted))
+
+    SGG = data['SGG_NM']
+    BJDONG = data['BJDONG_NM']
+    BLDG = data['BLDG_NM']
+
+
+    building_df = df[(df['SGG_NM'] == SGG) & (df['BJDONG_NM'] == BJDONG) & (df['BLDG_NM'] == BLDG)]
+    building_df = building_df.sort_values(by='DEAL_YMD', ascending=False)
+
+    date = list(building_df.iloc[:10, :]['DEAL_YMD'])
+    price = list(building_df.iloc[:10, :]['OBJ_AMT'])
+
+    return date, price
